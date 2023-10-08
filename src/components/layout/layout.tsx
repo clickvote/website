1fb5b90df4e55d40d1fad1467de756b66563d328
Nevo David <nevo@notu.co>
2023-10-08 23:44:19 +0700

    feat: metadata

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import { DefaultSeo } from 'next-seo';
 import * as React from 'react';
 
 import Footer from '@/components/landing/Footer';
@@ -12,6 +13,34 @@ export default function RootLayout({
 }) {
   return (
       <>
+          <DefaultSeo
+              title='Clickvote - Like, Upvote And Review Any Context'
+              description='Seamlessly Integrate Like, Upvote, and Review Components into Your App with React / Angular / Svelte / Vue'
+              additionalLinkTags={[
+                  {
+                      rel: 'icon',
+                      href: 'https://clickvote.dev/favicon.ico'
+                  },
+                  {
+                      rel: 'apple-touch-icon',
+                      href: 'https://clickvote.dev/images/apple-touch-icon.png',
+                      sizes: '76x76'
+              }]}
+              openGraph={{
+                  type: 'website',
+                  locale: 'en_IE',
+                  url: 'https://clickvote.dev',
+                  siteName: 'Clickvote',
+                  images: [
+                      {url: 'https://clickvote.dev/images/og.png', alt: 'Clickvote - Like, Upvote And Review Any Context'}
+                  ]
+              }}
+              twitter={{
+                  handle: '@clickvote',
+                  site: '@clickvote',
+                  cardType: 'summary_large_image',
+              }}
+          />
         <div className='max-w-screen font-primary relative overflow-clip'>
           <Navbar />
           {children}
