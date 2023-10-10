792a56d9b79123f631643e7bbfeb5c57f1723471
Nevo David <nevo@notu.co>
2023-10-10 22:40:42 +0700

    feat: remove console log

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -8,12 +8,4 @@ import { showLogger } from '@/constant/env';
  */
 export default function logger(object: unknown, comment?: string): void {
   if (!showLogger) return;
-
-  console.log(
-    '%c ============== INFO LOG \n',
-    'color: #22D3EE',
-    `${typeof window !== 'undefined' && window?.location.pathname}\n`,
-    `=== ${comment ?? ''}\n`,
-    object
-  );
 }
