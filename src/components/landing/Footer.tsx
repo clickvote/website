1c34598540a4d1e057e36bc4a67aaed2c68096b3
Nevo David <nevo@notu.co>
2023-10-15 16:08:01 +0700

    feat: fix footer

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -4,11 +4,7 @@ import * as React from 'react';
 import GithubBtn from '@/components/GithubBtn';
 import Logo from '@/components/Logo';
 import {
-  DiscordSvg,
   GithubSvg,
-  LinkedinSvg,
-  SocialXSvg,
-  YoutubeSvg,
 } from '@/components/svgs';
 
 import { extraUrls } from '@/constant/config';
@@ -60,32 +56,19 @@ export default function Footer() {
         <div className='flex flex-1 flex-col gap-10 text-[14px] text-white md:flex-row md:gap-12'>
           <div className='flex w-full max-w-[174px] flex-col gap-2'>
             <FootLink title='Blog' url='/blog' />
-            <FootLink title='Contributors' />
             <FootLink title='Pricing' url='/pricing' />
-            <FootLink title='Careers' />
-            <FootLink title='2022 Events' />
-            <FootLink title='OSS Friends' />
-          </div>
-          <div className='flex w-full max-w-[174px] flex-col gap-2'>
             <FootLink
-              title='Documentation'
-              url={extraUrls.doc}
-              target='_blank'
+                title='Documentation'
+                url={extraUrls.doc}
+                target='_blank'
             />
-            <FootLink title='Roadmap' />
-            <FootLink title='Providers' />
-            <FootLink title='Handbook' />
-            <FootLink title='Contact Us' />
-            <FootLink title='Press Kit' />
-            <FootLink title='Polishing Season' />
-            <FootLink title='Notification Directory' />
-            <FootLink title='OSS' />
           </div>
+          {/*<div className='flex w-full max-w-[174px] flex-col gap-2'>*/}
+
+          {/*</div>*/}
           <div className='flex w-full max-w-[174px] flex-col gap-2'>
-            <FootLink title='Terms of Use' />
-            <FootLink title='Privacy Policy' />
-            <FootLink title='DPA' />
-            <FootLink title='Status Page' />
+            <FootLink title='Terms of Use' url="/terms-of-use" />
+            <FootLink title='Privacy Policy'  url="/privacy-policy" />
           </div>
         </div>
         <div className='flex flex-col gap-[54px]'>
@@ -95,31 +78,33 @@ export default function Footer() {
             <SocialIcon url={extraUrls.github}>
               <GithubSvg width={24} height={24} />
             </SocialIcon>
-            <SocialIcon url={extraUrls.linkedin}>
-              <LinkedinSvg width={24} height={24} />
-            </SocialIcon>
-            <SocialIcon url={extraUrls.youtube}>
-              <YoutubeSvg width={24} height={24} />
-            </SocialIcon>
-            <SocialIcon url={extraUrls.discord}>
-              <DiscordSvg width={24} height={24} />
-            </SocialIcon>
-            <SocialIcon url={extraUrls.twitter}>
-              <SocialXSvg width={24} height={24} />
-            </SocialIcon>
+            {/*<SocialIcon url={extraUrls.linkedin}>*/}
+            {/*  <LinkedinSvg width={24} height={24} />*/}
+            {/*</SocialIcon>*/}
+            {/*<SocialIcon url={extraUrls.youtube}>*/}
+            {/*  <YoutubeSvg width={24} height={24} />*/}
+            {/*</SocialIcon>*/}
+            {/*<SocialIcon url={extraUrls.discord}>*/}
+            {/*  <DiscordSvg width={24} height={24} />*/}
+            {/*</SocialIcon>*/}
+            {/*<SocialIcon url={extraUrls.twitter}>*/}
+            {/*  <SocialXSvg width={24} height={24} />*/}
+            {/*</SocialIcon>*/}
           </div>
         </div>
       </div>
 
       <div className='mt-[55px] flex justify-between md:mt-[84px]'>
         <p className='text-[14px] leading-[19px] text-[#FFFFFFB2]'>
-          Ⓒ 2023 Clickvote
+          Ⓒ 2023 <Link href="/" className="hover:underline">Clickvote</Link>
         </p>
         <div className='flex items-center justify-center'>
           <p className='text-[15px] leading-[20px] text-[#FFFFFF99]'>
             Designed by&nbsp;
           </p>
-          <SuperFine className='mt-[5px] h-[14px] w-[67px]' />
+          <a href="https://superfine.studio" target="_blank">
+            <SuperFine className='mt-[5px] h-[14px] w-[67px]' />
+          </a>
         </div>
       </div>
     </div>
