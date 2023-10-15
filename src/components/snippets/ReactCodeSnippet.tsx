bd651435ca23d75786346833f3c5fefb4dc3be8b
Nevo David <nevo@notu.co>
2023-10-15 17:11:45 +0700

    feat: revert

--- src/components/snippets/ReactCodeSnippet.tsx
+++ src/components/snippets/ReactCodeSnippet.tsx
@@ -15,9 +15,27 @@ const ReactCodeSnippet = () => {
       <p>
         <span className='text-[#7A7A7A]'>2</span>
         <span className='mr-6' />
+        <span className='text-white'>import </span>
+        <span className='text-[#7A7A7A]'>{`{ ClickVoteComponent }`} </span>
+        <span className='text-white'>from </span>
+        <span className='text-[#AF46FF]'>{`'@clickvote/react'`}</span>
+        <span className='text-[#7A7A7A]'>; </span>
       </p>
       <p>
         <span className='text-[#7A7A7A]'>3</span>
+        <span className='mr-6' />
+        <span className='text-white'>import </span>
+        <span className='text-[#7A7A7A]'>{`{ LikeStyle }`} </span>
+        <span className='text-white'>from </span>
+        <span className='text-[#AF46FF]'>{`'@clickvote/react'`}</span>
+        <span className='text-[#7A7A7A]'>; </span>
+      </p>
+      <p>
+        <span className='text-[#7A7A7A]'>6</span>
+        <span className='mr-6' />
+      </p>
+      <p>
+        <span className='text-[#7A7A7A]'>4</span>
         <span className='mr-6' />
         <span className='text-white'>export const </span>
         <span className='text-[#B9FFF2]'>LikeExample</span>
@@ -28,14 +46,14 @@ const ReactCodeSnippet = () => {
         <span className='text-[#7A7A7A]'>{`{`}</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>4</span>
+        <span className='text-[#7A7A7A]'>5</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='text-white'>return </span>
         <span className='text-[#7A7A7A]'>(</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>5</span>
+        <span className='text-[#7A7A7A]'>6</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='mr-4' />
@@ -45,7 +63,7 @@ const ReactCodeSnippet = () => {
         <span className='text-white'>{`>`}</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>6</span>
+        <span className='text-[#7A7A7A]'>7</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='mr-4' />
@@ -64,7 +82,7 @@ const ReactCodeSnippet = () => {
         <span className='text-white'>{`>`}</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>7</span>
+        <span className='text-[#7A7A7A]'>8</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='mr-4' />
@@ -83,7 +101,7 @@ const ReactCodeSnippet = () => {
         <span className='text-[#7A7A7A]'>{`} />}`}</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>8</span>
+        <span className='text-[#7A7A7A]'>9</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='mr-4' />
@@ -94,7 +112,7 @@ const ReactCodeSnippet = () => {
         <span className='text-white'>{`>`}</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>9</span>
+        <span className='text-[#7A7A7A]'>10</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='mr-4' />
@@ -104,13 +122,13 @@ const ReactCodeSnippet = () => {
         <span className='text-white'>{`>`}</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>10</span>
+        <span className='text-[#7A7A7A]'>11</span>
         <span className='mr-6' />
         <span className='mr-4' />
         <span className='text-[#7A7A7A]'>);</span>
       </p>
       <p>
-        <span className='text-[#7A7A7A]'>11</span>
+        <span className='text-[#7A7A7A]'>12</span>
         <span className='mr-6' />
         <span className='text-[#7A7A7A]'>{`};`}</span>
       </p>
